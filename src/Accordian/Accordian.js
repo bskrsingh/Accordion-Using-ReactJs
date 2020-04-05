14326a0fa1953cbe75bd4cea50c74b0f5a93c6dd
bskrsingh <bskrsingh@gmail.com>
2020-04-05 06:38:47 -0400

    creating Accordian using Reactjs

---
+++ src/Accordian/Accordian.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import AccordianData from './AccordianData';
+import './Accordian.css'
+
+const accordian = () =>{
+    return (
+    <div className="accordian">
+    <AccordianData />
+    </div>
+    )
+}
+
+export default accordian
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Accordian from './Accordian/Accordian'
+
+function App() {
+  return (
+    <div className="App">
+     <Accordian />
+    </div>
+  );
+}
+
+export default App;
